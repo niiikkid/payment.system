99d4584a3e084ad7bd770e16cc53d7a866047877
niiikkid <niiikkid@gmail.com>
2025-11-01 08:37:51 +1000

    Refactor TronScanUsdtCommand to streamline balance retrieval by directly using TRX currency. Comment out unused transaction retrieval code for clarity.

--- app/Console/Commands/TronScanUsdtCommand.php
+++ app/Console/Commands/TronScanUsdtCommand.php
@@ -41,8 +41,10 @@ class TronScanUsdtCommand extends Command
 
         $balance = $blockchain->getAddressBalance($network, $currency, $address);
         $this->line('Баланс: ' . $money->format($balance) . ' ' . $currency->value);
-        $transactions = $blockchain->getIncomingTransactions($network, $currency, $address);
-        dump($transactions);
+        $balance = $blockchain->getAddressBalance($network, Currency::TRX, $address);
+        $this->line('Баланс: ' . $money->format($balance) . ' ' . Currency::TRX->value);
+        //$transactions = $blockchain->getIncomingTransactions($network, $currency, $address);
+       // dump($transactions);
 
         return self::SUCCESS;
     }
