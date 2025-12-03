911c78cd41c0be1f92c3c9f6a944386fe304fe20
niiikkid <niiikkid@gmail.com>
2025-12-03 11:09:28 +1000

    Update Layout.vue: modify header structure to include an SVG icon for enhanced visual appeal, adjust title styling for better readability, and remove unnecessary div for cleaner markup.

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -40,10 +40,14 @@ function confirmLogout() {
 </script>
 
 <template>
-    <div class="bg-base-200 px-4 py-6">
+    <div>
         <div class="mb-6">
-            <h1 class="text-2xl font-semibold">Настройки</h1>
-            <p class="text-base-content/70">Управляйте профилем и настройками аккаунта</p>
+            <h1 class="text-xl font-semibold flex items-center gap-2 py-0.5">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 opacity-60">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                </svg>
+                Настройки аккаунта
+            </h1>
         </div>
 
         <div class="flex flex-col lg:flex-row lg:space-x-12">
@@ -86,5 +90,5 @@ function confirmLogout() {
             </div>
         </div>
     </div>
-    
+
 </template>
