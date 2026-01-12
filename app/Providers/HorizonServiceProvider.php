0c11f880d62abe2019c654e155b59fbff8900641
niiikkid <niiikkid@gmail.com>
2026-01-13 02:41:59 +0700

    Enhance Horizon access control: update 'viewHorizon' gate definition to restrict access to users with the 'admin' role, improving security and authorization logic.

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
@@ -38,10 +39,12 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     protected function gate(): void
     {
-        Gate::define('viewHorizon', function ($user = null) {
-            return in_array(optional($user)->email, [
-                //
-            ]);
+        Gate::define('viewHorizon', function (?User $user = null) {
+            if (! $user) {
+                return false;
+            }
+
+            return $user->hasRole('admin');
         });
     }
 }
